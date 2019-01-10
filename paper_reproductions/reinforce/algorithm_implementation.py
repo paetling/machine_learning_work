2799a0060053ea919316a9b037469e13487166fc
Alex Etling <paetling@gmail.com>
2019-01-10 15:07:47 -0500

    this did not work.
    I was trying to only train the network on episodes that went longer than a certain time period. The hope was that it would learn what these networks were doing well and learn those values. What I found instead was that it basically learned to only go one direction at the end presumably because I was telling it the example cases were correct with similar cross entropy cost function and all of the examples failed in the end. It did not realize that it wanted to do better than systematically fail towards the end.
    
    I was getting pi values for the two actions in cart pole like the following:
    Entropy input:  [[2.3831616e-01 7.6168382e-01]
     [4.7573900e-01 5.2426094e-01]
     [7.2258633e-01 2.7741367e-01]
     [4.8678452e-01 5.1321542e-01]
     [7.3212337e-01 2.6787660e-01]
     [5.0059533e-01 4.9940470e-01]
     [2.7041581e-01 7.2958410e-01]
     [1.2395370e-01 8.7604636e-01]
     [2.8909466e-01 7.1090537e-01]
     [5.4213411e-01 4.5786586e-01]
     [3.0234945e-01 6.9765055e-01]
     [5.5807686e-01 4.4192320e-01]
     [3.1630281e-01 6.8369716e-01]
     [1.4830467e-01 8.5169536e-01]
     [6.4370260e-02 9.3562973e-01]
     [2.8186485e-02 9.7181356e-01]
     [6.9658995e-02 9.3034106e-01]
     [1.6942954e-01 8.3057046e-01]
     [7.2038822e-02 9.2796123e-01]
     [3.0667976e-02 9.6933204e-01]
     [1.3786635e-02 9.8621339e-01]
     [6.6899257e-03 9.9331009e-01]
     [3.5269307e-03 9.9647301e-01]
     [2.0181821e-03 9.9798179e-01]
     [1.2477082e-03 9.9875224e-01]
     [8.2824798e-04 9.9917173e-01]
     [5.8639218e-04 9.9941361e-01]]

--- paper_reproductions/reinforce/algorithm_implementation.py
+++ paper_reproductions/reinforce/algorithm_implementation.py
@@ -1,4 +1,5 @@
 import sys
+import statistics
 import tensorflow as tf
 from ..library import DenseLayer, create_dense_neural_net
 
@@ -40,23 +41,27 @@ class Reinforce:
 
         self.actions_to_take = tf.distributions.Categorical(probs=self.softmax).sample()
 
-        one_hot_actions = tf.one_hot(self.actions_taken, self.action_size)
-        self.base_cross_entropy = tf.reduce_sum(-tf.math.multiply(tf.math.log(1 - self.softmax), one_hot_actions), axis=1)
-        self.reward_weighted_cross_entropy = tf.math.multiply(self.discounted_episode_rewards, self.base_cross_entropy)
+        self.one_hot_actions = tf.one_hot(self.actions_taken, self.action_size)
+        self.base_cross_entropy = tf.reduce_sum(-tf.math.multiply(tf.math.log(self.softmax), self.one_hot_actions), axis=1)
+        # self.reward_weighted_cross_entropy = tf.math.multiply(self.discounted_episode_rewards, self.base_cross_entropy)
 
-        self.loss = -tf.reduce_sum(self.reward_weighted_cross_entropy)
+        self.loss = tf.reduce_sum(self.base_cross_entropy)
 
         self.train = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
     def train_model(self, number_of_batches, batch_size, max_steps):
+        episode_length_threshold = 25
+
         for batch_index in range(number_of_batches):
 
-            states = []
-            actions_taken = []
+            batch_states = []
+            batch_actions_taken = []
             batch_rewards = []
             batch_steps = []
             for episode_index in range(batch_size):
                 batch_rewards.append([])
+                batch_states.append([])
+                batch_actions_taken.append([])
                 batch_steps.append(0)
 
                 state = self.environment.reset()
@@ -68,38 +73,46 @@ class Reinforce:
 
                     batch_rewards[-1].append(reward)
                     batch_steps[-1] += 1
-                    states.append(state)
-                    actions_taken.append(action)
+                    batch_states[-1].append(state)
+                    batch_actions_taken[-1].append(action)
 
                     if done:
                         break
 
-
+            average_episode_length = statistics.mean([len(batch_reward) for batch_reward in batch_rewards])
             discounted_rewards = []
-            final_rewards = []
-            for batch_reward in batch_rewards:
+            above_threshold_actions_taken = []
+            states = []
+            actions_taken = []
+            for batch_reward_index in range(len(batch_rewards)):
+                batch_reward = batch_rewards[batch_reward_index]
                 current_reward = 0
 
-                for i in range(len(batch_reward)):
-                    reward = batch_reward[i]
-                    current_reward = current_reward * self.discount_factor + reward
-                    discounted_rewards.append(current_reward)
+                if len(batch_reward) > episode_length_threshold:
+                    above_threshold_actions_taken.append(len(batch_reward))
 
-                    if i == len(batch_reward) - 1:
-                        final_rewards.append(current_reward)
+                    for episode_index in range(len(batch_reward)):
+                        reward = batch_reward[episode_index]
+                        states.append(batch_states[batch_reward_index][episode_index])
+                        actions_taken.append(batch_actions_taken[batch_reward_index][episode_index])
+                        current_reward = current_reward * self.discount_factor + reward
+                        discounted_rewards.append(current_reward)
 
             # print('actions taken', actions_taken)
-            bce, loss,train = self.session.run([self.base_cross_entropy, self.loss, self.train], feed_dict={
-                                                                            self.input:states,
-                                                                            self.actions_taken:actions_taken,
-                                                                            self.discounted_episode_rewards: discounted_rewards
-                                                                            })
-            print('Training Batch: ', batch_index)
-            print('Base Cross Entroy: ', bce)
-            print('Final Rewards: ', final_rewards)
-            print('Loss: ', loss)
-            print('Average Number of Steps: ', sum(batch_steps)/len(batch_steps))
-            print('\n')
+            if len(above_threshold_actions_taken) > 0:
+                #episode_length_threshold = (5 * episode_length_threshold + statistics.mean(above_threshold_actions_taken)) / 6
+                bce, sm, one_a, loss,train = self.session.run([self.base_cross_entropy, self.softmax, self.one_hot_actions,self.loss, self.train], feed_dict={
+                                                                                self.input:states,
+                                                                                self.actions_taken:actions_taken,
+                                                                                self.discounted_episode_rewards: discounted_rewards
+                                                                                })
+                print('Entropy input: ', sm, one_a)
+                print('Training Batch: ', batch_index)
+                print('Base Cross Entroy: ', bce)
+                print('Average Actions Taken: ', average_episode_length,'   Threshold: ', episode_length_threshold, '  Training Set Actions Taken: ', above_threshold_actions_taken)
+                print('Loss: ', loss)
+                print('Average Number of Steps: ', sum(batch_steps)/len(batch_steps))
+                print('\n')
 
 
 
--- paper_reproductions/trainer.py
+++ paper_reproductions/trainer.py
@@ -4,4 +4,4 @@ from .reinforce.environments.cart_pole import CartPole
 cart_pole = CartPole(True)
 reinforce = Reinforce(cart_pole)
 
-reinforce.train_model(1000, 5, 1000)
+reinforce.train_model(10000, 10, 1000)
