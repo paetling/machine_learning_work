9cc729cbb949e8763b443d2ab136439f47346871
Alex Etling <paetling@gmail.com>
2019-01-10 16:37:00 -0500

    this attempts to fully implement the update described in this paper: http://www-anw.cs.umass.edu/~barto/courses/cs687/williams92simple.pdf
    It seems to work for the most part, but trains slowly and seems to not always train at all. Not sure what I would need to tweak to make it better

--- paper_reproductions/reinforce/algorithm_implementation.py
+++ paper_reproductions/reinforce/algorithm_implementation.py
@@ -11,8 +11,7 @@ class Reinforce:
         self.action_size = self.environment.get_number_of_actions()
 
         self.input = tf.placeholder(tf.float32, shape=[None, *self.state_size])
-        self.actions_taken = tf.placeholder(tf.int64, shape=[None])
-        self.discounted_episode_rewards = tf.placeholder(tf.float32, shape=[None])
+        self.discounted_episode_reward = tf.placeholder(tf.float32, shape=None)
 
         self.learning_rate = 2.5e-4
         self.discount_factor = 0.99
@@ -27,7 +26,8 @@ class Reinforce:
 
 
     def _create_learning_network(self, output_units):
-
+        # the idea is to manually do this update
+        # ∆Theta = learning_rate * r * ∆ln(pi)
         self.softmax = create_dense_neural_net(
             self.input,
             [
@@ -37,68 +37,48 @@ class Reinforce:
                 DenseLayer(output_units, activation=tf.keras.activations.softmax),
             ]
         )
+        self.log_probailites = tf.math.log(self.softmax)
 
         self.actions_to_take = tf.distributions.Categorical(probs=self.softmax).sample()
 
-        one_hot_actions = tf.one_hot(self.actions_taken, self.action_size)
-        self.base_cross_entropy = tf.reduce_sum(-tf.math.multiply(tf.math.log(1 - self.softmax), one_hot_actions), axis=1)
-        self.reward_weighted_cross_entropy = tf.math.multiply(self.discounted_episode_rewards, self.base_cross_entropy)
+        optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
+        self.gradients_and_vars = optimizer.compute_gradients(self.log_probailites)
+        self.old_vars = [pair[1] for pair in self.gradients_and_vars]
+        self.new_graidents = [pair[0] * (self.discounted_episode_reward * self.learning_rate) for pair in self.gradients_and_vars]
+        self.apply_gradients = optimizer.apply_gradients(zip(self.new_graidents, self.old_vars))
 
-        self.loss = -tf.reduce_sum(self.reward_weighted_cross_entropy)
-
-        self.train = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
     def train_model(self, number_of_batches, batch_size, max_steps):
         for batch_index in range(number_of_batches):
 
-            states = []
-            actions_taken = []
-            batch_rewards = []
-            batch_steps = []
+            discounted_rewards = 0
+            number_of_steps_in_batch = []
             for episode_index in range(batch_size):
-                batch_rewards.append([])
-                batch_steps.append(0)
+                number_of_steps_in_batch.append(0)
 
                 state = self.environment.reset()
 
                 for i in range(max_steps):
+                    number_of_steps_in_batch[-1] += 1
+
                     (actions_to_take,) = self.session.run([self.actions_to_take], feed_dict={self.input:[state]})
                     action = actions_to_take[0]
+                    old_state = state
                     state, reward, done = self.environment.step(action)
 
-                    batch_rewards[-1].append(reward)
-                    batch_steps[-1] += 1
-                    states.append(state)
-                    actions_taken.append(action)
+                    discounted_rewards = discounted_rewards * self.discount_factor + reward
+
+                    self.session.run([self.apply_gradients], feed_dict={
+                                                                            self.input:[old_state],
+                                                                            self.discounted_episode_reward: discounted_rewards
+                                                                            })
 
                     if done:
                         break
 
 
-            discounted_rewards = []
-            final_rewards = []
-            for batch_reward in batch_rewards:
-                current_reward = 0
-
-                for i in range(len(batch_reward)):
-                    reward = batch_reward[i]
-                    current_reward = current_reward * self.discount_factor + reward
-                    discounted_rewards.append(current_reward)
-
-                    if i == len(batch_reward) - 1:
-                        final_rewards.append(current_reward)
-
-            # print('actions taken', actions_taken)
-            bce, loss,train = self.session.run([self.base_cross_entropy, self.loss, self.train], feed_dict={
-                                                                            self.input:states,
-                                                                            self.actions_taken:actions_taken,
-                                                                            self.discounted_episode_rewards: discounted_rewards
-                                                                            })
             print('Training Batch: ', batch_index)
-            print('Base Cross Entroy: ', bce)
-            print('Final Rewards: ', final_rewards)
-            print('Loss: ', loss)
-            print('Average Number of Steps: ', sum(batch_steps)/len(batch_steps))
+            print('Average Number of Steps: ', sum(number_of_steps_in_batch)/len(number_of_steps_in_batch))
             print('\n')
 
 
